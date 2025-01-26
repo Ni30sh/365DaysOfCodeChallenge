000e468fc54a91db96eced9c2e14efc06f92f228
Ni30shkumar <addy2166@gmail.com>
2025-01-26 21:51:15 +0530

    code

--- Day15.py
+++ Day15.py
@@ -1,10 +1,3 @@
-#Add Binary Strings
-
-#Given two binary strings s1 and s2 consisting of only 0s and 1s.
-#Find the resultant string after adding the two Binary Strings.
-#Note: The input strings may contain leading zeros 
-#but the output string should not have any leading zeros.
-
 def add_binary_strings(s1, s2):
     # Convert binary strings to integers
     num1 = int(s1, 2)
